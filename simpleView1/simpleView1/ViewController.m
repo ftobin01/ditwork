f3497d7c797dec18b46039ab4bac96ae8f695ad0
F T <ftobin01@gmail.com>
2015-06-10 19:03:24 +0100

    w research 4

--- simpleView1/simpleView1/ViewController.m
+++ simpleView1/simpleView1/ViewController.m
@@ -11,9 +11,15 @@
 
 @interface ViewController ()
 @property (nonatomic, strong) UILabel   *speechLabel;
+@property (nonatomic, strong) UIBezierPath *bezierPath;
 @end
 
 @implementation ViewController
+
+static NSDictionary *commsInArray;
+static NSDictionary *commsOutArray;
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -23,106 +29,59 @@
     //[self.view.layer display];
  //   [self drawRect2:self.view.frame];
     [self setUpView];
+  //  UIGraphicsEndImageContext();
+    
+   [self showResponseOut : @"text Out"];
+        [self showResponseIn : @"text  In"];
+    [self drawDateBubble];
+    UIGraphicsEndImageContext();
 }
-
 -(void) setUpView
 {
 // (1) Creating a bitmap context, filling it with yellow as "background" color:
 CGSize size = CGSizeMake(self.view.bounds.size.width, self.view.bounds.size.height);
     UIGraphicsBeginImageContextWithOptions(CGSizeMake(size.width, size.height), YES, 0.0);
 [[UIColor yellowColor] setFill];
-    
 UIRectFill(CGRectMake(0, 0, size.width, size.height));
-    CGRect smallRect = CGRectMake(50,200,200,100);
-    CGRect smallRect3 =CGRectMake(57,100,200,100);
-// (2) Create a circle via a bezier path and stroking+filling it in the bitmap context:
-//
-  //  UIBezierPath *bezierPath = [UIBezierPath bezierPathWithArcCenter:CGPointMake(size.width/2, size.height/2) radius:140 startAngle:0 endAngle:2 * M_PI clockwise:YES];
-   
-    UIBezierPath *bezierPath = [UIBezierPath bezierPathWithRoundedRect:smallRect cornerRadius:4];
-    [[UIColor greenColor] setFill];
-    [bezierPath  fill];
+
+}
+
+-(void)showResponseOut : ( NSString *) textStr
+{
+    //CGRect smallRect = CGRectMake(50,200,200,100);
+    CGRect smallRect3 =CGRectMake(150,100,200,100);
+
+    //[ UIGraphicsGetCurrentContext()] fill];
+    _bezierPath = [UIBezierPath bezierPathWithRoundedRect:smallRect3 cornerRadius:4] ;
     
-    
-    //[desiredStrokeColor setStroke];
-    //[desiredFillColor setFill];
-     [[UIColor greenColor] setFill];
-    [bezierPath  fill];
-    bezierPath = [UIBezierPath bezierPathWithRoundedRect:smallRect3 cornerRadius:4];
+    UILabel *myLabel=[[UILabel alloc] initWithFrame:CGRectMake(0, 420, 320, 100)];
     CGPoint  t1=CGPointMake(smallRect3.origin.x+ smallRect3.size.width-2,smallRect3.origin.y);
     CGPoint t2=CGPointMake( smallRect3.origin.x+smallRect3.size.width+10,smallRect3.origin.y);
     CGPoint t3=CGPointMake( smallRect3.origin.x+smallRect3.size.width,smallRect3.origin.y+15);
     
-    [bezierPath moveToPoint: t1];
-    [bezierPath addLineToPoint:t2];
-    [bezierPath addLineToPoint:t3];
+    [_bezierPath moveToPoint: t1];
+    [_bezierPath addLineToPoint:t2];
+    [_bezierPath addLineToPoint:t3];
     
-    [[UIColor blueColor] setStroke];
-    [[UIColor blueColor] setFill];
-    CGPoint  tl1=CGPointMake(smallRect3.origin.x-2,smallRect3.origin.y);
-    CGPoint tl2=CGPointMake( smallRect3.origin.x-10,smallRect3.origin.y);
-    CGPoint tl3=CGPointMake( smallRect3.origin.x-10,smallRect3.origin.y+15);
+    [_bezierPath  closePath];
     
-    [bezierPath moveToPoint: t1];
-    [bezierPath addLineToPoint:t2];
-    [bezierPath addLineToPoint:t3];
-    
-    [bezierPath  closePath];
-    
-    [[UIColor greenColor] setFill];
-    [bezierPath  fill];
-    [bezierPath moveToPoint: tl1];
-    [bezierPath addLineToPoint:tl2];
-    [bezierPath addLineToPoint:tl3];
-    
-    
-   // [bezierPath addLineToPoint:t3];
-    [bezierPath  closePath];
-    bezierPath.lineWidth = 1.0;
-    [[UIColor blueColor] setFill];
-    [bezierPath fill];
-    [bezierPath stroke]  ;
-[[UIColor blackColor] setStroke];
+   
+    // [bezierPath addLineToPoint:t3];
+  //  [bezierPath  closePath];
+    _bezierPath.lineWidth = 0.0;
+ /*
+    [[UIColor blackColor] setStroke];
 bezierPath.lineWidth = 0.0;
-    
-[bezierPath stroke];
+   */
+[_bezierPath stroke];
 [[UIColor redColor] setFill];
-[bezierPath fill];
-    [[UIColor greenColor] setFill];
-   // [bezierPath2 fill];
-    
-/*
-// (3) Creating an array of images:
-NSArray *rocks = @[[UIImage imageNamed:@"rock1"],
-                   [UIImage imageNamed:@"rock2"],
-                   [UIImage imageNamed:@"rock3"],
-                   [UIImage imageNamed:@"rock4"],
-                   [UIImage imageNamed:@"rock5"],
-                   [UIImage imageNamed:@"rock6"],
-                   [UIImage imageNamed:@"rock7"],
-                   [UIImage imageNamed:@"rock8"],
-                   [UIImage imageNamed:@"rock9"]];
-
-// (4) Drawing rocks in a loop, each chosen randomly from the image set and drawn at a random position in a circular pattern:
-for ( int i = 0; i < 100; i++)
-{
-    int idx = arc4random() % rocks.count;
-    NSLog(@"idx = %d", idx);
-    int radius = 100;
-    int revolution = 360;
-    float r = (float)(arc4random() % radius);
-    float angle = (float)(arc4random() % revolution);
-    float x = size.width/2 + r * cosf(angle * M_PI/180.0);
-    float y = size.height/2 + r * sinf(angle * M_PI/180.0);
-    CGSize rockSize = ((UIImage *)rocks[idx]).size;
-    [rocks[idx] drawAtPoint:CGPointMake(x-rockSize.width/2, y-rockSize.height/2)];
-}
-
- */
+[_bezierPath fill];
+   
+[self drawText :  textStr: smallRect3];
 // (5) Deriving a new UIImage instance from the bitmap context:
 UIImage *fImg = UIGraphicsGetImageFromCurrentImageContext();
 // (6) Closing the context:
-UIGraphicsEndImageContext();
+//UIGraphicsEndImageContext();
 // (7) Setting the image view's image property to the created image, and displaying
 UIImageView *iv = [[UIImageView alloc] initWithImage:fImg];
 [self.view addSubview:iv];
@@ -130,13 +89,92 @@ UIImageView *iv = [[UIImageView alloc] initWithImage:fImg];
 }
 
 
+-(void) drawDateBubble
+{
+            //CGRect smallRect = CGRectMake(50,200,200,100);
+        CGRect smallRect3 =CGRectMake(200,300,20,50);
+        
+        //[ UIGraphicsGetCurrentContext()] fill];
+        _bezierPath = [UIBezierPath bezierPathWithRoundedRect:smallRect3 cornerRadius:4] ;
+    
+        
+        
+        // [bezierPath addLineToPoint:t3];
+        //  [bezierPath  closePath];
+        _bezierPath.lineWidth = 0.0;
+        /*
+         [[UIColor blackColor] setStroke];
+         bezierPath.lineWidth = 0.0;
+         */
+        [_bezierPath stroke];
+        [[UIColor redColor] setFill];
+        [_bezierPath fill];
+        
+        [self drawText :  @" 26 May 2015": smallRect3];
+        // (5) Deriving a new UIImage instance from the bitmap context:
+        UIImage *fImg = UIGraphicsGetImageFromCurrentImageContext();
+        // (6) Closing the context:
+        //UIGraphicsEndImageContext();
+        // (7) Setting the image view's image property to the created image, and displaying
+        UIImageView *iv = [[UIImageView alloc] initWithImage:fImg];
+        [self.view addSubview:iv];
+        
+    
+}
 
 
 
 
+-(void)showResponseIn: ( NSString *) textStr
+{
+    CGRect smallRect = CGRectMake(10,150,200,100);
+    //CGRect smallRect3 =CGRectMake(57,100,200,100);
+    
+    _bezierPath = [UIBezierPath bezierPathWithRoundedRect:smallRect cornerRadius:4];
+  /*
+    
+    CGPoint  tl1=CGPointMake(smallRect.origin.x-2,smallRect.origin.y);
+    CGPoint tl2=CGPointMake( smallRect.origin.x-10,smallRect.origin.y);
+    CGPoint tl3=CGPointMake( smallRect.origin.x-10,smallRect.origin.y+15);
+  
+    [_bezierPath moveToPoint: tl1];
+    [_bezierPath addLineToPoint:tl2];
+    [_bezierPath addLineToPoint:tl3];
+    
+   
+    // [bezierPath addLineToPoint:t3];
+    
+   
+   [_bezierPath  closePath];
+   */
+   
+   _bezierPath.lineWidth = 0.0;
+   
+    
+    [_bezierPath stroke];
+   
+    [[UIColor greenColor] setFill];
+    [_bezierPath fill];
+    
+    [self drawText : textStr : smallRect];
+    // (5) Deriving a new UIImage instance from the bitmap context:
+    UIImage *fImg = UIGraphicsGetImageFromCurrentImageContext();
+    // (6) Closing the context:
+  //  UIGraphicsEndImageContext();
+    // (7) Setting the image view's image property to the created image, and displaying
+    UIImageView *iv = [[UIImageView alloc] initWithImage:fImg];
+    [self.view addSubview:iv];
+    
+  
+}
 
 
-
+-(void) makePhotoCircle
+{
+// (2) Create a circle via a bezier path and stroking+filling it in the bitmap context:
+//
+//  UIBezierPath *bezierPath = [UIBezierPath bezierPathWithArcCenter:CGPointMake(size.width/2, size.height/2) radius:140 startAngle:0 endAngle:2 * M_PI clockwise:YES];
+}
 
 
 
@@ -160,7 +198,7 @@ UIImageView *iv = [[UIImageView alloc] initWithImage:fImg];
     [self.view addSubview: _speechLabel];
 }
 
-
+/*
 
 -(void)makeLabel2
 {
@@ -191,6 +229,8 @@ myLabel.backgroundColor = [UIColor greenColor];
     [self.view addSubview:myLabel];
 }
 
+*/ 
+ 
 -(void)makeLabel3
 {
     
@@ -208,6 +248,84 @@ myLabel.backgroundColor = [UIColor greenColor];
     
     [self.view addSubview:imgView];
 }
+
+
+-(void)drawText : (NSString *) textStr : (CGRect) drawRect
+{
+    
+   // CGContextRef *currentContext = UIGraphicsGetCurrentContext();
+// Create text attributes
+NSDictionary *textAttributes = @{NSFontAttributeName: [UIFont systemFontOfSize:18.0]};
+
+// Create string drawing context
+NSStringDrawingContext *drawingContext = [[NSStringDrawingContext alloc] init];
+drawingContext.minimumScaleFactor = 0.2; // Half the font size
+    UILabel *label = [[UILabel alloc] initWithFrame: drawRect];
+    label.font = [UIFont systemFontOfSize: 18.0];
+    CGSize maxSize = CGSizeMake(label.frame.size.width,MAXFLOAT);
+    
+CGRect labelRect = [textStr boundingRectWithSize:maxSize options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName: label.font} context:drawingContext];
+    
+    NSLog(@"size %@", NSStringFromCGSize(labelRect.size));
+    
+    
+    
+    
+//CGRect drawRect = CGRectMake(0.0, 0.0, 200.0, 100.0);
+[textStr drawWithRect:labelRect
+             options:NSStringDrawingUsesLineFragmentOrigin
+          attributes:textAttributes
+             context:drawingContext];
+}
+
+
+/* Creates an image with a home-grown graphics context, burns the supplied string into it. */
+/*
+- (UIImage *)burnTextIntoImage:(NSString *)text :(UIImage *)img {
+    
+    NSFont *font = [NSFont fontWithName:@"Palatino-Roman" size:14.0];
+    
+    NSDictionary *attrsDictionary =
+    
+    [NSDictionary dictionaryWithObjectsAndKeys:
+     font, NSFontAttributeName,
+     [NSNumber numberWithFloat:1.0], NSBaselineOffsetAttributeName, nil]
+    
+    
+    
+    
+    UIGraphicsBeginImageContext(img.size);
+    
+    CGRect aRectangle = CGRectMake(0,0, img.size.width, img.size.height);
+    [img drawInRect:aRectangle];
+    
+    [[UIColor redColor] set];           // set text color
+    NSInteger fontSize = 14;
+    if ( [text length] > 200 ) {
+        fontSize = 10;
+    }
+    UIFont *font = [UIFont boldSystemFontOfSize: fontSize];     // set text font
+    
+    [ text drawInRect : aRectangle                      // render the text
+             withFont : font
+        lineBreakMode : NSLineBreakByTruncatingTail  // clip overflow from end of last line
+            alignment : NSTextAlignmentCenter ];
+    
+    UIImage *theImage=UIGraphicsGetImageFromCurrentImageContext();   // extract the image
+    UIGraphicsEndImageContext();     // clean  up the context.
+    return theImage;
+}
+
+*/
+
+
+
+
+
+
+
+
+
 
 /*
 - (void)drawRect:(CGRect)rect
@@ -259,7 +377,7 @@ myLabel.backgroundColor = [UIColor greenColor];
 
 
 
-
+/*
 -(void)drawRect2:(CGRect)rect {
     // Drawing code
     UILabel *myLabel=[[UILabel alloc] initWithFrame:CGRectMake(0, 420, 320, 100)];
@@ -306,13 +424,18 @@ myLabel.backgroundColor = [UIColor greenColor];
     UIGraphicsEndImageContext();
 }
 
+ 
+*/
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-
-
+-(void) makeTestComms
+{
+    commsInArray = [[NSDictionary alloc] initWithObjectsAndKeys:
+                    @"10:15",@"Hello", nil];
+}
 
 
 @end
